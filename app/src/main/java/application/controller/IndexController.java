1a063b73d4e1ae3fbce3e3a66e8a202aca8dbf2f
Beatriz <BEATRIZ.CRUZ84@ETEC.SP.GOV.BR>
2024-03-15 10:35:53 +0000

    save1

--- app/src/main/java/application/controller/IndexController.java
+++ app/src/main/java/application/controller/IndexController.java
@@ -3,10 +3,14 @@ package application.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ch.qos.logback.core.model.Model;
+
 @Controller
 public class IndexController {
     @RequestMapping("/")
-    public String index() {
+    public String index(Model ui) {
+        String msg = "Olá controller!!!";
+        ui.addAttribute ("message", msg); 
         return "/index.jsp";
     }
 }
