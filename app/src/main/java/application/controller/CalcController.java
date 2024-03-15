a144ca6f96ed1955fe0d1864d3ebed5fc0aa51e3
Beatriz <BEATRIZ.CRUZ84@ETEC.SP.GOV.BR>
2024-03-15 12:21:00 +0000

    save1

--- app/src/main/java/application/controller/CalcController.java
+++ app/src/main/java/application/controller/CalcController.java
@@ -1,12 +1,24 @@
 package application.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CalcController {
-        @RequestMapping(/calc)
+        @RequestMapping("/calc")
         public String form() {
             return "calc/form";
     }
-}
+
+    @RequestMapping("/soma")
+    public String soma(Model ui, 
+         @RequestParam("numA") int nA,
+         @RequestParam("numB") int nB) {
+    ui.addAttribute("resultado", nA + nB);
+    return "calc/form";
+        } 
+    }
+
+   
